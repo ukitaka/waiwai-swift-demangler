b45624ab4f987c69963a2e88af20b65a17ee9d11
ukitaka <yuki.takahashi.1126@gmail.com>
2018-12-16 11:44:08 +0900

    Fix test

--- Tests/SwiftDemanglerTests/SwiftDemanglerTests.swift
+++ Tests/SwiftDemanglerTests/SwiftDemanglerTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class SwiftDemanglerTests: XCTestCase {
     func testEx1() {
         XCTAssertEqual(demangle(name: "$S13ExampleNumber6isEven6numberSbSi_tF"),
+                       "ExampleNumber.isEven(number: Swift.Int) -> Swift.Bool")
     }
 
 
